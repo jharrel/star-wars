8288d6ad947abf6cfdba6f9e7d0e9371db54dc83
jharrel <harrel.jason@gmail.com>
2020-04-25 20:33:47 -0700

    added semantic UI additions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar'
-import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Menu, Container, Dimmer, Loader } from 'semantic-ui-react';
 
 function App() {
   const [people, setPeople] = useState([]);
@@ -24,9 +28,32 @@ function App() {
     fetchPlanets();
   }, []);
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Container>
+          {loading ? (
+            <Dimmer active inverte4d>
+              <Loader inverted>
+                Loading
+              </Loader>
+            </Dimmer>
+          ) : (
+            <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/people'>
+              <People />
+            </Route>
+            <Route exact path='/planets'>
+              <Planets />
+            </Route>
+          </Switch>
+          ) }
+        </Container>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Menu, Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <Menu inverted>
             <Container>
-                <Menu.Item name="star wars API" />
-                <Menu.Item name="People" />
-                <Menu.Item name="Planets" />
+                <Link to='/'>
+                    <Menu.Item name="star wars API" />
+                </Link>
+                <Link to='people'>
+                    <Menu.Item name="People" />
+                </Link>
+                <Link to='planets'>
+                    <Menu.Item name="Planets" />
+                </Link>
             </Container>
         </Menu>
     )
